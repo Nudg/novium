3b67e1c4c398f96cf3c1663ba42627a53fe3209e
Moto JoJo <Moto@Nudge.local>
2016-08-12 14:38:16 +0300

    Intro Page add to Novium

---
+++ intro/js/custom.js
@@ -0,0 +1,20 @@
+/*Theme    : Quick
+ * Author  : Design_mylife
+ * Version : V1.0
+ *
+ */
+
+
+// ***********************************
+// Backstretch - Slider on Background
+// ***********************************
+
+$(document).ready (function(){
+$('#intro-container').backstretch([
+   "intro/images/showcase-1.jpg",
+   "intro/images/showcase-2.jpg",
+   "intro/images/showcase-3.jpg",
+   "intro/images/showcase-4.jpg",
+], {duration: 5000, fade: 1000});
+
+});
